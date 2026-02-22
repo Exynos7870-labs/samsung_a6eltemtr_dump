461cef1f3eb3cfcad34eb4b2cf20b835c4499305
Flominator <github.flominatorgd@gmail.com>
2026-02-22 22:09:26 +0100

    Add extras for a6eltemtr-user 10 QP1A.190711.020 A600T1UVS8CUA1 release-keys
    Signed-off-by: Flominator <github.flominatorgd@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## a6eltemtr-user 10 QP1A.190711.020 A600T1UVS8CUA1 release-keys
+- Manufacturer: samsung
+- Platform: universal7884
+- Codename: a6eltemtr
+- Brand: samsung
+- Flavor: a6eltemtr-user
+- Release Version: 10
+- Id: QP1A.190711.020
+- Incremental: A600T1UVS8CUA1
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: samsung/a6eltemtr/a6eltemtr:10/QP1A.190711.020/A600T1UVS8CUA1:user/release-keys
+- OTA version: 
+- Branch: a6eltemtr-user-10-QP1A.190711.020-A600T1UVS8CUA1-release-keys
+- Repo: samsung_a6eltemtr_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat system/system/app/SBrowser_11.0/SBrowser_11.0.apk.* 2>/dev/null >> system/system/app/SBrowser_11.0/SBrowser_11.0.apk
+rm -f system/system/app/SBrowser_11.0/SBrowser_11.0.apk.* 2>/dev/null
+cat system/system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/priv-app/Velvet/Velvet.apk
+rm -f system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null >> system/system/priv-app/StickerFaceAR/StickerFaceAR.apk
+rm -f system/system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null
